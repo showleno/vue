99995fc08ed324c3599c9ff6e9f2427f68d60fe9
laughing <32934545+showleno@users.noreply.github.com>
2018-04-24 18:36:21 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import Home from '@/components/Home'
-import Boys from '@/components/boys'
-import List from '@/components/List'
+// import Boys from '@/components/boys'
+// import List from '@/components/List'
+const Boys = () => import(/*webpackChunkName: "Boys"*/'@/components/boys');
+const List = () => import(/*webpackChunkName: "List"*/'@/components/List');
 import StartPage from '@/components/StartPage'
 
 Vue.use(Router)
