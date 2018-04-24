b96c2eb89e0e450e247b0f86400350542819ecaa
laughing <32934545+showleno@users.noreply.github.com>
2018-04-24 18:35:33 +0800

    Add files via upload

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,6 +2,30 @@
   <main class="main_content">
     <div class="list_wrapper">
         <h3>我是列表页</h3>
+        <div class="search_wrapper">
+            <div class="search_input">
+              <font-icon id="icon--fangdajing" class="icon"></font-icon>
+              <input type="text" placeholder="搜索商品" class="search">
+            </div>
+        </div>
+        <div class="content">
+          <div class="nav_tab">
+            <ul>
+              <li v-for="list in tabList" :key="list.id">
+                <span>{{ list.text }}</span>
+              </li>
+            </ul>
+          </div>
+          <div class="list_wrapper">
+            <ul class="left">
+              <li></li>
+            </ul>
+            <ul class="right">
+              <li></li>
+            </ul>
+          </div>
+        </div>
+        
     </div>
     <Footer :pathName='pathName' />
   </main>
@@ -15,6 +39,20 @@ export default {
   },
   data () {
     return {
+      tabList: [
+        {
+          "text": "Boys"
+        },
+        {
+          "text": "Girls"
+        },
+        {
+          "text": "Kids"
+        },
+        {
+          "text": "Lifestyle"
+        }
+      ],
       pathName: [
             {'url': '/boys', "title": '首页', "path": "home", "icon": "icon-shouye"},
             {'url': '/list', "title": '分类', "path": "list", "icon": "icon-biaoqian" },
